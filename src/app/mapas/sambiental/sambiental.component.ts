df04ed517b48c61da046faa4f9a95657c85e3e42
Vulnerabilidade Ambiental <dayonne.p@gmail.com>
2017-08-20 17:36:56 -0300

    arrumando os mapas

--- src/app/mapas/sambiental/sambiental.component.ts
+++ src/app/mapas/sambiental/sambiental.component.ts
@@ -5,11 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sambiental.component.html',
   styleUrls: ['./sambiental.component.css']
 })
-export class SambientalComponent implements OnInit {
+export class SambientalComponent implements OnInit { 
 
   constructor() { }
 
   ngOnInit() {
   }
+  imagem01='./assets/Mapas/Saude Ambiental/bacias-hidrograficas-1.jpg'; 
+  imagem02='./assets/Mapas/Saude Ambiental/carencia-habitacional-agua-1.jpg'; 
+  imagem03='./assets/Mapas/Saude Ambiental/carencia-habitacional-esgoto-1.jpg'; 
+  imagem04='./assets/Mapas/Saude Ambiental/carencia-habitacional-lixo-1.jpg'; 
+  imagem05='./assets/Mapas/Saude Ambiental/carencia-habitacional-1.jpg'; 
+
 
 }

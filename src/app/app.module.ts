8376bd67a56155eeed40f1333bfa37da42621f96
Rafael Fronteira <dayonne.p@gmail.com>
2017-11-04 16:06:20 -0200

    obs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 // import dos componentes das rotas principais
-import { EquipeComponent } from './equipe/equipe.component';
-import { ProjetoComponent } from './projeto/projeto.component';
-import { MetodologiaComponent } from './metodologia/metodologia.component';
-import { MaterialApoioComponent } from './material-apoio/material-apoio.component';
-import { BlogComponent } from './blog/blog.component';
-import { LoginComponent } from './login/login.component';
+import { MaterialApoioComponent } from './components/material-apoio/material-apoio.component';
+import { MetodologiaComponent } from './components/metodologia/metodologia.component';
+import { ProjetoComponent } from './components/projeto/projeto.component';
+import { EquipeComponent } from './components/equipe/equipe.component';
 
 
 // rotas, servicos e modulos
 
-import { AppRoutingModule } from './app.routng.module';
-import { MapasComponent } from './mapas/mapas.component';
+import { MapasComponent } from "./components/mapas/mapas.component";
+import { ModuleWithProviders } from '@angular/core';
 
 
+const appRouters: Routes = [
+    { path: '', component: ProjetoComponent },
+    { path: 'mapas', component: MapasComponent  },
+    { path: 'material', component: MaterialApoioComponent },
+    { path: 'equipe', component: EquipeComponent },
+    { path: 'metodologia', component: MetodologiaComponent }
+  
+];
 
 
 
@@ -28,14 +34,16 @@ import { MapasComponent } from './mapas/mapas.component';
     EquipeComponent,
     ProjetoComponent,
     MetodologiaComponent,
-    MaterialApoioComponent,
-    BlogComponent,
-    LoginComponent
-  ],
+    MaterialApoioComponent
+      ],
   imports: [
+    RouterModule.forRoot(appRouters),
 
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+  
+    ],
+    exports: [
+      RouterModule
     ],
   bootstrap: [AppComponent]
 })

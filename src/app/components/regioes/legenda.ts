3f35db550ec21c8fc300e22d4a9fe3357012085c
Rafael Fronteira <dayonne.p@gmail.com>
2018-02-16 13:36:35 -0200

    organizando as legendas

--- src/app/components/regioes/legenda.ts
+++ src/app/components/regioes/legenda.ts
@@ -4,7 +4,7 @@ export let _Legenda: any[] = [
         {
             id: 1,
             title:['Legenda','Legenda','Legenda','Legenda','Legenda','Legenda',],
-            leg: ['leg1 ','leg1','leg2','leg4','leg5','leg6','leg7'],
+            leg: ['leg3 ','leg1','leg2'],
             
         }, 
         {

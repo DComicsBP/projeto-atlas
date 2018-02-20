8a57f99776fa246e50bf767141f5aa95dc6626db
Rafael Fronteira <dayonne.p@gmail.com>
2018-02-20 13:01:45 -0300

    só vai

--- src/app/components/regioes/demografia.ts
+++ src/app/components/regioes/demografia.ts
@@ -10,9 +10,9 @@ export let _MapasDemografia: any[] = [
      
     },
     {
-      titulo:'Titulo 02',
-      nome:'Mapa 02',
-      link:'https://fusiontables.google.com/embedviz?q=select+col22+from+1IcoXZ7XHA4uhvqXAP9n3fjjsN6eJ1uWfOIH3Xgc3&viz=MAP&h=false&lat=-30.191385705216614&lng=-50.62162998193355&t=1&z=8&l=col22&y=2&tmplt=2&hml=KML',
+      titulo:'Taxa de População Raça/Cor - Branca',
+      nome:'Taxa Pop. Branca 2010',
+      link:'https://fusiontables.google.com/embedviz?q=select+col75+from+1uiEJp2KXWckyDPSxltk98wABquJ5cad-vgdgzZUL&viz=MAP&h=false&lat=-30.38599268750699&lng=-53.90326060937501&t=1&z=6&l=col75&y=2&tmplt=2&hml=KML',
       Ltitulo:['0,0 - 2,2','2,2 - 140,2','140,3 - 533,1','533,2 - 1166,4','1166,5 - 2.918,9'],
       leg: ['leg1 ','leg2','leg3','leg4','leg5' ],
             },

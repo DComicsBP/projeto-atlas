671289a9f0c7001a33967347a95698030a4adf3b
DComicsBP <dayonne.p@gmail.com>
2018-04-29 22:03:58 -0300

    ultimas alteracoes nos textos

--- src/app/components/material-apoio/router02.ts
+++ src/app/components/material-apoio/router02.ts
@@ -1,7 +1,84 @@
-// tslint:disable-next-line:eofline
+// tslint:disable:eofline
+// tslint:disable:max-line-length
 export let router02 = [
-                {nome: `2dsd`, link: `link02`},
-                {nome: `2dsd`, link: `link02`},
-                {nome: `2dsd`, link: `link01`},
-                {nome: `2dsd`, link: `link01`}
-            ];
+                          {
+                            nome: `Saúde e ambiente no processo de desenvolvimento`,
+                            link: `https://drive.google.com/file/d/1Vm0J1pZ_POkhWl55ufCRFioqpmuBdFun/view?usp=sharing`,
+                            resumo: 'No ano de 2002 perdemos nosso grande companheiro Milton Santos, intelectual brilhante, cuja obra seminal ultrapassou as fronteiras muitas gerações, na forma de pensar a geografia e a sociedade. A presença de Milton...'
+                          },
+                          {
+                            nome: `Saúde Pública e Meio Ambiente: evolução do
+                            conhecimento e da prática, alguns aspectos
+                            éticos`,
+                            link: `https://drive.google.com/file/d/1mDVFC4iPHMFZQQZKgRdx12pCiSZcleys/view?usp=sharing`,
+                            resumo:
+                              `O artigo enfoca a relação saúde-meio ambiente e a área
+                              de Saúde Ambiental. Apresenta as definições adotadas
+                              pela Organização Mundial da Saúde para a Saúde
+                              Ambiental, destacando sua preocupação tanto com
+                              estudos para verificar as possíveis relações entre os ...`
+                        },
+                        {
+                          nome: `Ecologia política, economia ecológica e saúde
+                          coletiva: interfaces para a sustentabilidade do
+                          desenvolvimento e para a promoção da saúde`,
+                          link: `https://drive.google.com/file/d/1Siqu0ehdl-lIZhRc-8XI6I5w1SXcmfsN/view?usp=sharing`,
+                          resumo: `Este artigo apresenta as contribuições fornecidas
+                          pela economia ecológica e pela ecologia política
+                          para entendermos a relação entre desenvolvi-
+                          mento econômico, a degradação sócio-ambien-
+                          tal e o papel da saúde coletiva. Isso se torna ainda
+                          mais ...`
+                        },
+                          {
+                            nome: `Saúde, ambiente e desenvolvimento: reflexões
+                            sobre a experiência da COPASAD – Conferência
+                            Pan-Americana de Saúde e Ambiente no Contexto
+                            do Desenvolvimento Sustentável`,
+                            link: `https://drive.google.com/file/d/17MduNs48MEBwaFslwFcS66KZgDpWb79u/view?usp=sharing`,
+                            resumo: `Este artigo apresenta uma reflexão
+                            sobre a relação saúde, ambiente e desenvolvi-
+                            mento no contexto da Saúde Pública. Aborda a
+                            possibilidade de implementação de políticas
+                            públicas no país concernentes a esta vasta te-
+                            mática, tendo por referência uma avaliação crí-
+                            tica da experiência brasileira frente à Confe-
+                            rência Pan-americana de Saúde e Ambiente
+                            no Contexto do Desenvolvimento Sustentável
+                            – COPASAD.`},
+                          {
+                            nome: `Sistemas de indicadores
+                            de saúde e ambiente em instituições de saúde`,
+                            link: `https://drive.google.com/file/d/1DON1NlPtheHpd7dvIcEjIVx9fjkSj8lv/view?usp=sharing`,
+                            resumo: `Neste artigo, é realizada uma revisão
+                            teórica sobre aspectos conceituais e metodológicos
+                            da construção de um sistema de indicadores, vi-
+                            sando a gestão integrada em saúde e ambiente nas
+                            instituições de pesquisa, ensino e prestação de ser-
+                            viços em saúde, que articule também a Biossegu-
+                            rança. Partiu-se do estudo de mosdelos internacio-
+                            nais de indicadores, destacando-se o modelo da
+                            Organização Mundial de Saúde, mais apropria-
+                            do para este trabalho, a partir do qual foi feito o
+                            processo de coleta, organização e síntese de infor-
+                            mações. Visa-se criar um instrumental de moni-
+                            toramento e avaliação destas ações, a fim de apoiar ...`
+                          },
+                          {
+                            nome: `Relação entre saúde e saneamento
+                            na perspectiva do desenvolvimento`,
+                            link: `https://drive.google.com/file/d/1FrnK7B1tcm4kcAeeQn3cA7swMrkcD3v1/view?usp=sharing`,
+                            resumo: `Discute-se a relação entre saúde e sa-
+                            neamento, situando-a no contexto do processo
+                            de desenvolvimento social. É defendida inicial-
+                            mente a inserção dessa relação no atual enfo-
+                            que saúde e ambiente, reconhecendo que esta
+                            nova área não elimina a pertinência da abor-
+                            dagem saúde-saneamento, na verdade sua pre-
+                            cursora. Apresenta-se a conceituação de sanea-
+                            mento e sua atual situação no país, além dos
+                            marcos conceituais da relação saúde-sanea-
+                            mento.`
+                          }
+
+                      ];
--- src/app/components/material-apoio/router03.ts
+++ src/app/components/material-apoio/router03.ts
@@ -1,7 +1,14 @@
-// tslint:disable-next-line:eofline
+// tslint:disable:eofline
+// tslint:disable:max-line-length
 export let router03 = [
-    {nome: `3dsd`, link: `link02`},
-    {nome: `3dsd`, link: `link02`},
-    {nome: `3dsd`, link: `link01`},
-    {nome: `3dsd`, link: `link01`}
-];
+                          {
+                            nome: `Atualização da lista de causas de mortes evitáveis (5 a 74 anos de idade) por intervenções do Sistema Único de Saúde do Brasil`,
+                            link: `https://drive.google.com/file/d/1Pf85WrFf3iJh2JXih5gjxMO0USKrKTox/view?usp=sharing`,
+                            resumo: 'As causas de mortes evitáveis ou reduzíveis são definidas como aquelas preveníveis, total ou parcialmente, por ações efetivas dos serviços de saúde que estejam acessíveis em um determinado local e época. Essas causas devem ser revisadas à luz da evolução do conhecimento e tecnologia para ...'
+                          },
+                          {
+                            nome: `Notas sobre Mapas`,
+                            link: `https://drive.google.com/file/d/1Sr7veSRWSZAcRkUwchaDzKkuQFDN09UR/view?usp=sharing`,
+                            resumo: `<br>Consultas de dados nos sistemas de informação, principalmente o Sistema de Informação sobre Mortalidade – SIM e o Sistema de Informação sobre Nascidos Vivos – SINASC, disponíveis a partir das bases públicas no sítio do Departamento de Informática do SUS – Datasus, durante o período de execução do edital SEAD 23 (fevereiro de 2016 até fevereiro de 2017). Os dados estão sujeitos a revisão.`
+                          }
+                      ];
